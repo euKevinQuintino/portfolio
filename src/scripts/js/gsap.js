206bed7beb07107f55f9b2bf669bc15caad2f355
KEVIN NICOLAS BUENO QUINTINO <kevin.quintino@fatec.sp.gov.br>
2021-04-03 14:21:48 -0300

    2.0.2

--- src/scripts/js/gsap.js
+++ src/scripts/js/gsap.js
@@ -1,46 +1,45 @@
+//gsap.registerPlugin(ScrollTrigger);
+//above the fold
 gsap.from(".above-the-fold__message", {
   opacity: 0,
   duration: 1.5,
-  y: -69,
-  ease: "Expo.easeInOut",
+  y: -64,
 });
 
 gsap.from(".above-the-fold__top", {
   opacity: 0,
-  x: -69,
-  duration: 0.69,
-  ease: "Power1.easeInOut",
-  delay: 1.24,
+  x: -64,
+  duration: 0.64,
+  delay: 1.28,
 });
 
 gsap.from(".above-the-fold__check-more", {
   opacity: 0,
-  y: 69,
-  duration: 0.69,
-  delay: 1.24,
+  y: 64,
+  duration: 0.64,
+  delay: 1.28,
 });
 
+//resume
 gsap.from(".under-the-fold__resume", {
   scrollTrigger: {
     trigger: ".under-the-fold__resume",
     start: "top bottom",
   },
   opacity: 0,
-  x: 69,
+  x: 64,
   duration: 0.96,
 });
 
 gsap.from(".under-the-fold__summary__row__left-column", {
-  scrollTrigger: ".under-the-fold__resume",
-  start: "top center",
+  scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
   opacity: 0,
   y: 100,
   duration: 0.96,
 });
 
 gsap.from(".under-the-fold__summary__row__middle-column", {
-  scrollTrigger: ".under-the-fold__resume",
-  start: "top center",
+  scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
   opacity: 0,
   y: 100,
   duration: 0.96,
@@ -48,17 +47,19 @@ gsap.from(".under-the-fold__summary__row__middle-column", {
 });
 
 gsap.from(".under-the-fold__summary__row__right-column", {
-  scrollTrigger: ".under-the-fold__resume",
-  start: "top center",
+  scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
   opacity: 0,
   y: 100,
   duration: 0.96,
   delay: 0.124,
 });
 
+//contact
 gsap.from(".under-the-fold__contact-me__content", {
-  scrollTrigger: ".under-the-fold__contact-me__content",
-  start: "top bottom",
+  scrollTrigger: {
+    trigger: ".under-the-fold__contact-me__content",
+    start: "top bottom",
+  },
   opacity: 0,
   duration: 0.96,
 });
@@ -66,26 +67,60 @@ gsap.from(".under-the-fold__contact-me__content", {
 gsap.from(".back-to-top", {
   scrollTrigger: ".under-the-fold__summary",
   opacity: 0,
-  y: 69,
+  y: 64,
   duration: 0.64,
 });
 
-/*gsap.from(".footer__social-media", {
+//achievements
+gsap.from(".under-the-fold__achievements__graduations__item", {
   scrollTrigger: {
-    trigger: ".footer__social-media",
+    trigger: ".under-the-fold__achievements__graduations",
     start: "top bottom",
   },
   opacity: 0,
+  x: -64,
+  duration: 0.96,
+});
+
+gsap.from(".under-the-fold__achievements__certifications__item", {
+  scrollTrigger: {
+    trigger: ".under-the-fold__achievements__certifications",
+    start: "top bottom",
+  },
+  opacity: 0,
+  x: -64,
+  duration: 0.96,
+  delay: 0.64,
+});
+
+gsap.from(".under-the-fold__achievements__books__item", {
+  scrollTrigger: {
+    trigger: ".under-the-fold__achievements__books",
+    start: "top bottom",
+  },
+  opacity: 0,
+  y: 64,
+  duration: 1.24,
+  delay: 0.64,
+});
+
+//footer
+/*gsap.from(".footer__social-media", {
+  scrollTrigger: {
+    trigger: ".footer__social-media",
+    start: "top bottom"
+  },
+  opacity: 0,
   y: 256,
-  duration: 0.64,
+  duration: 0.64
 });*/
 
 /*gsap.from(".footer__message", {
   scrollTrigger: {
     trigger: ".under-the-fold__contact-me__content__location",
-    start: "top center",
+    start: "top center"
   },
   opacity: 0,
   y: 256,
-  duration: 0.64,
+  duration: 0.64
 });*/
--- src/scripts/js/scroll.js
+++ src/scripts/js/scroll.js
@@ -1,14 +1,12 @@
-var scrollDown = function () {
+var toggleBackToTop = function () {
   if (window.scrollY <= 69) {
     document.querySelector(".back-to-top").style.display = "none";
   } else {
     document.querySelector(".back-to-top").style.display = "block";
   }
 };
+window.addEventListener("scroll", toggleBackToTop);
 
-window.addEventListener("scroll", scrollDown);
-
-function scrollToSecondSection(id){
+function ScrollTo(id) {
   document.getElementById(id).scrollIntoView(true);
 }
-
