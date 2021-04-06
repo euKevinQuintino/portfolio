16ddf24c4ad04efc4073e483a111ddd962a91638
KEVIN NICOLAS BUENO QUINTINO <kevin.quintino@fatec.sp.gov.br>
2021-04-05 21:28:28 -0300

    2.0.3

--- src/scripts/js/gsap.js
+++ src/scripts/js/gsap.js
@@ -1,126 +1,129 @@
-//gsap.registerPlugin(ScrollTrigger);
-//above the fold
-gsap.from(".above-the-fold__message", {
-  opacity: 0,
-  duration: 1.5,
-  y: -64,
-});
+/*$(window).on("load", function() {
+//$(document).ready(function() {
+  gsap.registerPlugin(ScrollTrigger);
+  //above the fold
+  gsap.from(".above-the-fold__message", {
+    opacity: 0,
+    duration: 1.28,
+    y: -64,
+  });
 
-gsap.from(".above-the-fold__top", {
-  opacity: 0,
-  x: -64,
-  duration: 0.64,
-  delay: 1.28,
-});
+  gsap.from(".above-the-fold__top", {
+    opacity: 0,
+    x: -64,
+    duration: 0.64,
+    delay: 1.28,
+  });
 
-gsap.from(".above-the-fold__check-more", {
-  opacity: 0,
-  y: 64,
-  duration: 0.64,
-  delay: 1.28,
-});
+  gsap.from(".above-the-fold__check-more", {
+    opacity: 0,
+    y: 64,
+    duration: 0.64,
+    delay: 1.28,
+  });
 
-//resume
-gsap.from(".under-the-fold__resume", {
-  scrollTrigger: {
-    trigger: ".under-the-fold__resume",
-    start: "top bottom",
-  },
-  opacity: 0,
-  x: 64,
-  duration: 0.96,
-});
+  //resume
+  gsap.from(".under-the-fold__resume", {
+    scrollTrigger: {
+      trigger: ".under-the-fold__resume",
+      start: "top bottom",
+    },
+    opacity: 0,
+    x: 64,
+    duration: 0.96,
+  });
 
-gsap.from(".under-the-fold__summary__row__left-column", {
-  scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
-  opacity: 0,
-  y: 100,
-  duration: 0.96,
-});
+  gsap.from(".under-the-fold__summary__row__left-column", {
+    scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
+    opacity: 0,
+    y: 100,
+    duration: 0.96,
+  });
 
-gsap.from(".under-the-fold__summary__row__middle-column", {
-  scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
-  opacity: 0,
-  y: 100,
-  duration: 0.96,
-  delay: 0.064,
-});
+  gsap.from(".under-the-fold__summary__row__middle-column", {
+    scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
+    opacity: 0,
+    y: 100,
+    duration: 0.96,
+    delay: 0.064,
+  });
 
-gsap.from(".under-the-fold__summary__row__right-column", {
-  scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
-  opacity: 0,
-  y: 100,
-  duration: 0.96,
-  delay: 0.124,
-});
+  gsap.from(".under-the-fold__summary__row__right-column", {
+    scrollTrigger: { trigger: ".under-the-fold__resume", start: "top center" },
+    opacity: 0,
+    y: 100,
+    duration: 0.96,
+    delay: 0.124,
+  });
 
-//contact
-gsap.from(".under-the-fold__contact-me__content", {
-  scrollTrigger: {
-    trigger: ".under-the-fold__contact-me__content",
-    start: "top bottom",
-  },
-  opacity: 0,
-  duration: 0.96,
-});
+  //contact
+  gsap.from(".under-the-fold__contact-me__content", {
+    scrollTrigger: {
+      trigger: ".under-the-fold__contact-me__content",
+      start: "top bottom",
+    },
+    opacity: 0,
+    duration: 0.96,
+  });
 
-gsap.from(".back-to-top", {
-  scrollTrigger: ".under-the-fold__summary",
-  opacity: 0,
-  y: 64,
-  duration: 0.64,
-});
+  gsap.from(".back-to-top", {
+    scrollTrigger: ".under-the-fold__summary",
+    opacity: 0,
+    y: 64,
+    duration: 0.64,
+  });
 
-//achievements
-gsap.from(".under-the-fold__achievements__graduations__item", {
-  scrollTrigger: {
-    trigger: ".under-the-fold__achievements__graduations",
-    start: "top bottom",
-  },
-  opacity: 0,
-  x: -64,
-  duration: 0.96,
-});
+  //achievements
+  gsap.from(".under-the-fold__achievements__graduations__item", {
+    scrollTrigger: {
+      trigger: ".under-the-fold__achievements__graduations",
+      start: "top bottom",
+    },
+    opacity: 0,
+    x: -64,
+    duration: 0.96,
+  });
 
-gsap.from(".under-the-fold__achievements__certifications__item", {
-  scrollTrigger: {
-    trigger: ".under-the-fold__achievements__certifications",
-    start: "top bottom",
-  },
-  opacity: 0,
-  x: -64,
-  duration: 0.96,
-  delay: 0.64,
-});
+  gsap.from(".under-the-fold__achievements__certifications__item", {
+    scrollTrigger: {
+      trigger: ".under-the-fold__achievements__certifications",
+      start: "top bottom",
+    },
+    opacity: 0,
+    x: -64,
+    duration: 0.96,
+    delay: 0.64,
+  });
 
-gsap.from(".under-the-fold__achievements__books__item", {
-  scrollTrigger: {
-    trigger: ".under-the-fold__achievements__books",
-    start: "top bottom",
-  },
-  opacity: 0,
-  y: 64,
-  duration: 1.24,
-  delay: 0.64,
-});
+  gsap.from(".under-the-fold__achievements__books__item", {
+    scrollTrigger: {
+      trigger: ".under-the-fold__achievements__books",
+      start: "top bottom",
+    },
+    opacity: 0,
+    y: 64,
+    duration: 1.24,
+    delay: 0.64,
+  });
 
-//footer
-/*gsap.from(".footer__social-media", {
-  scrollTrigger: {
-    trigger: ".footer__social-media",
-    start: "top bottom"
-  },
-  opacity: 0,
-  y: 256,
-  duration: 0.64
-});*/
+  //footer
+  gsap.from(".footer__social-media", {
+    scrollTrigger: {
+      trigger: ".footer__social-media",
+      start: "top bottom"
+    },
+    opacity: 0,
+    y: 256,
+    duration: 0.64
+  });
 
-/*gsap.from(".footer__message", {
-  scrollTrigger: {
-    trigger: ".under-the-fold__contact-me__content__location",
-    start: "top center"
-  },
-  opacity: 0,
-  y: 256,
-  duration: 0.64
-});*/
+  gsap.from(".footer__message", {
+    scrollTrigger: {
+      trigger: ".under-the-fold__contact-me__content__location",
+      start: "top center"
+    },
+    opacity: 0,
+    y: 256,
+    duration: 0.64
+  });
+})*/
